0c3fe5ebb5416347894dbce124bd31134f28e652
Tom Smith <tom@craftscale.uk>
2024-12-05 13:16:26 +0000

    Created product and customer models
    Also added commenting

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+  define Customer class with attributes of name, email, phone and date created
+  models.Model means that this class is a model and django will create a table in the database for this model
+"""
+
+class Customer(models.Model): 
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    """ 
+        def __str__(self): defines what will be displayed as the title on django admin page, eg
+        a product on the django admin page will consist of {self.name} and {self..price}
+    """
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+    
+
+"""
+  define Product class with attributes of name, price, and description
+"""
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=500)
+
+    """ 
+        def __str__(self): defines what will be displayed as the title on django admin page, eg
+        a product on the django admin page will consist of {self.name} and {self..price}
+    """
+
+    def __str__(self):
+        return f"{self.name} ({self.price})" 
