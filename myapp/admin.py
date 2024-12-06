fe5e1b9d239ddda0ad216362f300105137e3d575
Tom Smith <tom@craftscale.uk>
2024-12-06 09:42:11 +0000

    Updated admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Customer, Product
 
 # Register your models here.
+
+
+"""
+  this section defines how the information will be displayed on the admin
+  page. for example, list_display defines which rows will be displayed on the
+  main admin info db page.
+"""
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'date_created')
+    list_filter = ('date_created')
+
+"""
+  this does the same but for products on the admin view.
+"""
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    list_filter = ('price')
+
